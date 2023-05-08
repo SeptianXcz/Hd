94a5352c4d6b4f099900add3ea61e0b820406923
SeptianXcz <109943864+SeptianXcz@users.noreply.github.com>
2023-05-08 12:09:25 +0700

    Update neoxrApi.js

--- system/neoxrApi.js
+++ system/neoxrApi.js
@@ -3,7 +3,7 @@ module.exports = class NeoxrApi {
    apiKey = null
 
    constructor(apiKey) {
-      this.apiKey = apiKey || ''
+      this.apiKey = apiKey || 'Zc9jqd'
    }
    
    check = async () => {
